80dbff0b4b998a3ec84471f4df046459640eccce
Isaac <160370245+ESRaiden@users.noreply.github.com>
2025-11-10 15:46:00 -0600

    Fix import case for User model

--- server/models/index.js
+++ server/models/index.js
@@ -1,5 +1,5 @@
 const { sequelize } = require('../config/database');
-const User = require('./User');
+const User = require('./user');
 const Client = require('./Client');
 const Folio = require('./Folio');
 const FolioEditHistory = require('./FolioEditHistory');
