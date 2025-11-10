5a7936c2611395a8cc79253da10528ab6a8e780e
Isaac <160370245+ESRaiden@users.noreply.github.com>
2025-11-10 15:50:34 -0600

    Rename Client import to match file casing

--- server/models/index.js
+++ server/models/index.js
@@ -1,6 +1,6 @@
 const { sequelize } = require('../config/database');
 const User = require('./user');
-const Client = require('./Client');
+const Client = require('./client');
 const Folio = require('./Folio');
 const FolioEditHistory = require('./FolioEditHistory');
 const Commission = require('./Commission');
